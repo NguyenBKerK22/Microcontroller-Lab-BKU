6e67aa5c4e17397bf7def1c46cd9b09cfe95ea33
Do Hoan Nguyen <dohoannguyen123@gmail.com>
2024-09-30 17:28:07 +0700

    Nothing

--- STM32_MCU_Lab2/Core/Src/main.c
+++ STM32_MCU_Lab2/Core/Src/main.c
@@ -166,8 +166,9 @@ void update7SEG(int index){
 	switch (index){
  	 case 0:
  //Display the first 7SEG with led_buffer[0]
-
  		 HAL_GPIO_WritePin(EN0_GPIO_Port,EN0_Pin,RESET);
+ 		 HAL_GPIO_WritePin(EN1_GPIO_Port,EN1_Pin,SET);
+ 		 HAL_GPIO_WritePin(EN2_GPIO_Port,EN2_Pin,SET);
  		 HAL_GPIO_WritePin(EN3_GPIO_Port,EN3_Pin,SET);
  		 break;
  	 case 1:
